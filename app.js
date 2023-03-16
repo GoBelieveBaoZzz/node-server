b2226ef66f1d7f86980df2c4b3a5d83d5de88a66
竟然有人抢包 <47656506+GoBelieveBaoZzz@users.noreply.github.com>
2023-03-16 16:26:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000, function () {
-    console.log('app listening on port 3000')
+app.listen(3001, function () {
+    console.log('app listening on port 3001')
 })
 
 //引入路由器模块
